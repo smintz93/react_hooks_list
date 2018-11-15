0ce9e704e8516bbbfc0bf127426b35e156e7ed5d
Sam <sammintz1993@gmail.com>
2018-11-15 13:54:57 -0600

    create functional app comp

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// experimenting with react hooks
+import React, { useState } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+function App(){
+  const [todos, setTodos] = useState([
+    { text: "Learn about React",
+      isCompleted: false 
+    },
+    { text: "Meet friend for lunch",
+      isCompleted: false 
+    },
+    { text: "Build really cool todo app",
+      isCompleted: false 
+    }
+  ]);
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        
       </div>
-    );
-  }
+    </div>
+
+  );
 }
 
 export default App;
