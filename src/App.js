b6f5e02cd0620cba743839b3aeacdab9094a4fad
Sam <sammintz1993@gmail.com>
2018-11-16 12:39:32 -0600

    can remove todos

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 import React, { useState } from 'react';
 import './App.css'
 
-function Todo({ todo, index, completeTodo })  {
+function Todo({ todo, index, completeTodo, deleteTodo })  {
   return(
     <div style={{textDecoration: todo.isCompleted ? "line-through" : "" }}
     className="todo">{todo.text}
     <div>
       <button onClick={() => completeTodo(index)}>Complete</button>
+      <button onClick={() => deleteTodo(index)}>x</button>
     </div>
     </div>
   )
@@ -54,16 +55,22 @@ function App(){
   };
 
   const completeTodo = (index) => {
-    const newTodos = [...todos]
+    const newTodos = [...todos];
     newTodos[index].isCompleted = true;
     setTodos(newTodos);
+  }
+
+  const deleteTodo = (index) => {
+     const newTodos = [...todos];
+     newTodos.splice(index, 1);
+     setTodos(newTodos);
   }
 
   return (
     <div className="app">
       <div className="todo-list">
           {todos.map((todo, index) => (
-            <Todo key={index} index={index} todo={todo} completeTodo={completeTodo}/>
+            <Todo key={index} index={index} todo={todo} completeTodo={completeTodo} deleteTodo={deleteTodo}/>
           ))}
           <TodoForm addToto={addToto}/>  
       </div>
