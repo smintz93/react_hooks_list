06df97823ca94d92e2bb2cfe9dbc0fa30c69ea56
Sam <sammintz1993@gmail.com>
2018-11-16 12:37:11 -0600

    linethrough when complete

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@
 import React, { useState } from 'react';
 import './App.css'
 
-function Todo({ todo, index })  {
+function Todo({ todo, index, completeTodo })  {
   return(
-    <div className="todo">{todo.text}</div>
+    <div style={{textDecoration: todo.isCompleted ? "line-through" : "" }}
+    className="todo">{todo.text}
+    <div>
+      <button onClick={() => completeTodo(index)}>Complete</button>
+    </div>
+    </div>
   )
 }
 
@@ -46,13 +51,19 @@ function App(){
   const addToto = (text) => {
     const newTodos = [...todos, { text }]
     setTodos(newTodos)
+  };
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos]
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
   }
 
   return (
     <div className="app">
       <div className="todo-list">
           {todos.map((todo, index) => (
-            <Todo key={index} index={index} todo={todo}/>
+            <Todo key={index} index={index} todo={todo} completeTodo={completeTodo}/>
           ))}
           <TodoForm addToto={addToto}/>  
       </div>
